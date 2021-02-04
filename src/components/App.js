d41d26b2851d3b9dbf2780cb36848b3601904cc0
Descifle <descifle_itb@yahoo.com>
2021-02-03 22:47:16 -0500

    added some more basics

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import { Container } from '@material-ui/core'
 import React from 'react'
 import '../css/main.scss'
 import Header from './Header'
+import Landing from './Landing'
 
 const App = () => {
     return (
         <React.Fragment>
             <Header />
+            <Landing />
             <Container>
-                yolo
+                yeeted
            </Container>
         </React.Fragment>  
     )
---
+++ src/components/Landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import healthyBanner from '../assets/healthy.jpg'
+// import mobileBanner from '../assets/mobile-healthy.jpg'
+import '../css/landing.scss'
+import Grid from '@material-ui/core/Grid'
+
+const Landing = () => {
+    return (
+        <div className="landing">
+            <Grid container justify="center" alignItems="center" spacing={3}>
+                <Grid className="text-center" item xs={8}>
+                    <h2>Fitness starts with what you eat.</h2>
+                    <p>Take control of your health</p>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default Landing
