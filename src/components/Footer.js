c7f679bc42426f182b8fd4e8fd8bebc1bbfbe4ed
Descifle <descifle_itb@yahoo.com>
2021-02-17 20:32:28 -0500

    adding some more content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,51 @@
 import React from 'react'
-import { Grid, Container } from '@material-ui/core'
+import { Grid, Container, Typography } from '@material-ui/core'
+import { Facebook, LinkedIn } from '@material-ui/icons'
 
 const Footer = () => {
     return (
         <footer>
             <Container className="footer-inner">
                 <div>
-                   <nav>
-                    <ul class="footer-inner_nav">
-                        <li><a href="#something">About</a></li>
-                        <li><a href="#something">Lorem Ipsum</a></li>
-                        <li><a href="#somethign">Sitemap</a></li>
-                    </ul>
-                   </nav>
+                    <nav>
+                        <ul className="footer-inner_nav">
+                            <li><a href="#something">About</a></li>
+                            <li><a href="#something">Lorem Ipsum</a></li>
+                            <li><a href="#somethign">Sitemap</a></li>
+                        </ul>
+                    </nav>
                 </div>
-                <Grid>
-                    <Grid></Grid>
-                    <Grid></Grid>
-                    <Grid></Grid>
+                <hr />
+                <Grid container >
+                    <Grid item xs={12} sm={3}>
+                        <Typography variant="h4">Lorem</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <Typography variant="h4">Lorem</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <Typography variant="h4">Lorem</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                        <Typography>ipsum</Typography>
+                    </Grid>
                 </Grid>
-                <div>
-                   <div>Social Media</div> 
-                   <div>Terms privacy policy etc</div>
+                <hr />
+                <div className="flex-between">
+                    <span className="flex-between">
+                        <Facebook className="icon" />
+                        <LinkedIn className="icon" />
+                    </span> 
+                    <span><span>Terms of Service</span> <span>Privacy Policy</span> <span>Descifle &#8482;</span>privacy policy etc</span>
                 </div>
             </Container>
         </footer>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -22,8 +22,8 @@ const Landing = () => {
                     <div className="text-center info-container">
                         <h1 variant="h1">Fitness starts with what you eat.</h1>
                         <div className="supercenter">
-                            <Typography style={{ fontFamily: "'Quattrocento Sans', sans-serif", textTransform: "initial" }} variant="button">Take control of your health.</Typography>
-                            <Button style={{ marginLeft: '2rem', color: 'white', border: 'white solid 1px', backgroundColor: 'rgba(186, 181, 181, 0.3)' }} variant="outlined">Create Account</Button>
+                            <Typography className="info-cta" style={{ fontFamily: "'Quattrocento Sans', sans-serif", textTransform: "initial" }} variant="button">Take control of your health.</Typography>
+                            <Button style={{ color: 'white', border: 'white solid 1px', backgroundColor: 'rgba(186, 181, 181, 0.3)' }} variant="outlined">Create Account</Button>
                         </div>
                     </div>
                 </Grid>
@@ -32,35 +32,41 @@ const Landing = () => {
             <div className="hero-border"></div>
             <Container className="landing-content">
                 <Grid container justify="center" alignItems="center">
-                    <Grid className="landing-content_main" item xs={12} sm={5} container alignItems="center">
-                        <h2>Lorem  <br />ipsum dolor sit.</h2>
-                        <p>
-                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                    <Grid className="landing-content_container" container justify="center" alignItems="center">
+                        <Grid className="landing-content_main" item xs={12} sm={5} >
+                            <h2>Diet</h2>
+                            <p>
+                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
                         </p>
-                        <span>link to info</span>
+                            <span>link to info</span>
+                        </Grid>
+                        <Grid className="landing-content-img" item xs={12} sm={5} >
+                            <img src="https://via.placeholder.com/150" alt="" />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={5} container justify="center" alignItems="center">
-                        <img src="https://via.placeholder.com/150" alt="" />
-                    </Grid>
-                    <Grid item xs={12} sm={5} container justify="center" alignItems="center">
-                        <img src="https://via.placeholder.com/150" alt="" />
-                    </Grid>
-                    <Grid className="landing-content_secondary" item xs={12} sm={5} container alignItems="center">
-                        <h2>Lorem  <br />ipsum dolor sit.</h2>
-                        <p>
-                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                    <Grid className="landing-content_container" container justify="center" alignItems="center">
+                        <Grid className="landing-content-img" item xs={12} sm={5} >
+                            <img src="https://via.placeholder.com/150" alt="" />
+                        </Grid>
+                        <Grid className="landing-content_secondary" item xs={12} sm={5} >
+                            <h2>Lorem  <br />ipsum dolor sit.</h2>
+                            <p>
+                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
                         </p>
-                        <span>link to info</span>
+                            <span>link to info</span>
+                        </Grid>
                     </Grid>
-                    <Grid className="landing-content_main" item xs={12} sm={5} container alignItems="center">
-                        <h2>Lorem  <br />ipsum dolor sit.</h2>
-                        <p>
-                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                    <Grid className="landing-content_container" container justify="center" alignItems="center">
+                        <Grid className="landing-content_main" item xs={12} sm={5}>
+                            <h2>Lorem  <br />ipsum dolor sit.</h2>
+                            <p>
+                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
                         </p>
-                        <span>link to info</span>
-                    </Grid>
-                    <Grid item xs={12} sm={5} container justify="center" alignItems="center">
-                        <img src="https://via.placeholder.com/150" alt="" />
+                            <span>link to info</span>
+                        </Grid>
+                        <Grid className="landing-content-img" item xs={12} sm={5} >
+                            <img src="https://via.placeholder.com/150" alt="" />
+                        </Grid>
                     </Grid>
                 </Grid>
             </Container>
